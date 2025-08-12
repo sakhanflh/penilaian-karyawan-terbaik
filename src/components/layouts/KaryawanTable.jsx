a901d7737a8edf9e6d8b618c7c21a4144c701239
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2025-08-12 11:49:31 +0700

    bug: bug on karyawan table

--- src/components/layouts/KaryawanTable.jsx
+++ src/components/layouts/KaryawanTable.jsx
@@ -1,7 +1,7 @@
 const KaryawanTable = ({ karyawan, onEdit, onDelete }) => {
     return (
-        <div className="overflow-hidden border border-gray-200 rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
+        <div className="overflow-x-scroll border border-gray-200 rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200 ">
                 <thead className="bg-gray-50">
                     <tr>
                         <th scope="col" className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
